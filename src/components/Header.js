7546f07525616227c37ceb8333cdc6f738f13bec
monferon <sidelnikov.ns@gmail.com>
2020-12-06 18:48:21 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        className="logo"
+        src="https://cdn.worldvectorlogo.com/logos/rockstar-games.svg"
+        alt="Nothing"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://photo.mac69.com/JPG-180506/180506_257/Bt9Pq00jEh_small.jpg"></img>
+      </div>
+      <div>
+        <img src="https://img.douxie.com/upload/imgs/1211/1352272842946.jpg"></img>{" "}
+        Description
+      </div>
+      <div>
+        My posts
+        <div>New Post</div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+      Main Content
+    </div>
+  );
+};
+
+export default Profile;
