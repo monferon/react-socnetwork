2cf0002f8a49591a5ccbbb9cf7fcea06f6d4e210
monferon <sidelnikov.ns@gmail.com>
2020-12-10 21:47:54 +0300

    upd navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,16 +6,19 @@ const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a>Profile</a>
+        <a href="/profile">Profile</a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a>Messages</a>
+        <a href="/dialogs">Messages</a>
       </div>
       <div className={s.item}>
-        <a>News</a>
+        <a href="/news">News</a>
       </div>
       <div className={s.item}>
-        <a>Contacts</a>
+        <a href="/music">Music</a>
+      </div>
+      <div className={s.item}>
+        <a href="/settings">Settings</a>
       </div>
     </nav>
   );
