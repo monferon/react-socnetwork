eb5f27486e5c51c8fca55ff0a976811431eb2f69
monferon <sidelnikov.ns@gmail.com>
2020-12-22 21:39:31 +0300

    add 'store'

--- src/index.js
+++ src/index.js
@@ -3,23 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import state, { subscribe, updateNewPostText } from "./redux/state";
-import { addPost } from "./redux/state";
+import store from "./redux/state";
 import "./index.css";
 
 let renderEntireTree = () => {
   ReactDOM.render(
     <React.StrictMode>
       <App
-        state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        state={store.getState()}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
       />
     </React.StrictMode>,
     document.getElementById("root")
   );
 };
 
-renderEntireTree(state);
+renderEntireTree(store.getState());
 
-subscribe(renderEntireTree);
+store.subscribe(renderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,57 +1,59 @@
-let renderEntireTree = () => {
-  console.log("state changed");
-};
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how a re you?!", likesCount: 0 },
-      { id: 2, message: "Hi, how a re you?!", likesCount: 15 },
-      { id: 3, message: "it's my first post!", likesCount: 5 },
-      { id: 4, message: "it's my first post!", likesCount: 7 },
-      { id: 5, message: "YO5!", likesCount: 23 },
-      { id: 6, message: "YO6!", likesCount: 65 },
-    ],
-    newPostText: "itka.com",
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how a re you?!", likesCount: 0 },
+        { id: 2, message: "Hi, how a re you?!", likesCount: 15 },
+        { id: 3, message: "it's my first post!", likesCount: 5 },
+        { id: 4, message: "it's my first post!", likesCount: 7 },
+        { id: 5, message: "YO5!", likesCount: 23 },
+        { id: 6, message: "YO6!", likesCount: 65 },
+      ],
+      newPostText: "itka.com",
+    },
+    dialogsPage: {
+      messages: [
+        { id: 1, message: "YO1!" },
+        { id: 2, message: "YO2!" },
+        { id: 3, message: "YO3!" },
+        { id: 4, message: "YO4!" },
+        { id: 5, message: "YO5!" },
+        { id: 6, message: "YO6!" },
+      ],
+      dialogs: [
+        { id: 1, name: "Kit" },
+        { id: 2, name: "Kit1" },
+        { id: 3, name: "Kit2" },
+        { id: 4, name: "Kit3" },
+        { id: 5, name: "Kit4" },
+        { id: 6, name: "Kit5" },
+      ],
+    },
+    sidebar: {},
   },
-  dialogsPage: {
-    messages: [
-      { id: 1, message: "YO1!" },
-      { id: 2, message: "YO2!" },
-      { id: 3, message: "YO3!" },
-      { id: 4, message: "YO4!" },
-      { id: 5, message: "YO5!" },
-      { id: 6, message: "YO6!" },
-    ],
-    dialogs: [
-      { id: 1, name: "Kit" },
-      { id: 2, name: "Kit1" },
-      { id: 3, name: "Kit2" },
-      { id: 4, name: "Kit3" },
-      { id: 5, name: "Kit4" },
-      { id: 6, name: "Kit5" },
-    ],
+  getState() {
+    return this._state;
   },
-  sidebar: {},
+  _callSubscriber() {
+    console.log("state changed");
+  },
+  addPost(postMessage) {
+    let newPost = {
+      id: 8,
+      message: this._state.profilePage.newPostText,
+      likesCount: 15,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 };
 
-export const addPost = (postMessage) => {
-  let newPost = {
-    id: 8,
-    message: state.profilePage.newPostText,
-    likesCount: 15,
-  };
-  state.profilePage.posts.push(newPost);
-  renderEntireTree();
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  renderEntireTree();
-};
-
-export const subscribe = (observer) => {
-  renderEntireTree = observer;
-};
-
-export default state;
+export default store;
+window.store = store;
